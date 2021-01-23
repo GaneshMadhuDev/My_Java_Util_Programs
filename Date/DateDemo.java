84994de002f951e6aa12ac196a70adb57c1832e6
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-23 10:29:10 -0600

    Usage of Date Demo Program and Tests

--- Date/DateDemo.java
+++ Date/DateDemo.java
@@ -1,15 +1,27 @@
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class JavaDemo {
+public class DateDemo {
 
+    public long newDate() throws ParseException {
 
-    public void newDate() {
+        long currentMilliSeconds = new SimpleDateFormat("yyyy-MM-dd").parse("1990-05-10").getTime();
 
-        Date date = new Date(1990, 3, 22);
+        System.out.println("Current Milliseconds since Given Date " + currentMilliSeconds);
 
+        return currentMilliSeconds;
 
+    }
+
+    public static Date parseDate(String date) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
 }
