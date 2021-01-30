dc6aa754532dedc604486812238e551422d2337b
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-30 16:41:54 -0600

    Ading Java Doc for LinkedList Usage

--- LinkedList/LinkedListDemo.java
+++ LinkedList/LinkedListDemo.java
@@ -4,6 +4,14 @@ import java.util.LinkedList;
 /**
  * Created by ganeshmadhu on 1/30/21.
  */
+
+/**
+ * which is a linear data structure where the elements are not stored in contiguous locations
+ * and every element is a separate object with a data part and address part.
+ * The elements are linked using pointers and addresses.
+ * Each element is known as a node.
+ * Due to the dynamicity and ease of insertions and deletions, they are preferred over the arrays.
+ */
 public class LinkedListDemo {
 
     public void linkedListDemoExample() {
