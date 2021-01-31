bce9b39762a2e5512c40634f0016b7e2de97f750
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-31 09:38:55 -0600

    Random Util Class Usage Demo

---
+++ Random/RandomDemo.java
@@ -0,0 +1,23 @@
+/**
+ * Created by ganeshmadhu on 1/30/21.
+ */
+
+/**
+ * Random(): Creates a new random number generator
+ *
+ * Random(long seed): Creates a new random number generator using a single long seed
+ *
+ * java.util.Random.doubles(): Returns an effectively unlimited stream of pseudo random double values, each between zero (inclusive) and one (exclusive)
+ *
+ * java.util.Random.ints(): Returns an effectively unlimited stream of pseudo random int values
+ *
+ * java.util.Random.longs(): Returns an effectively unlimited stream of pseudo random long values
+ *
+ * java.util.Random.nextDouble(): Returns the next pseudo random, uniformly distributed double value between 0.0 and 1.0 from this random number generator’s sequence
+ *
+ * java.util.Random.nextBoolean(): Returns the next pseudo random, uniformly distributed boolean value from this random number generator’s sequence
+ *
+ *
+ */
+public class RandomDemo {
+}
