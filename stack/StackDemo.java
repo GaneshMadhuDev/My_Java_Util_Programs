bd10ce86e26d4e10b0112636a01e7f1a655d282f
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-31 10:12:57 -0600

    Stack demo Usage -> push, pop, search, peek()

--- stack/StackDemo.java
+++ stack/StackDemo.java
@@ -2,6 +2,8 @@
  * Created by ganeshmadhu on 1/30/21.
  */
 
+import java.util.Stack;
+
 /**
  * use the Stack() constructor of this class. The below example creates an empty Stack.
  *
@@ -18,6 +20,29 @@
 public class StackDemo {
 
 
+    public void stackDemoExample() {
+
+        Stack<Integer> myNumbersStack  = new Stack<Integer>();
+
+        for  (int number = 0; number < 50; number++) {
+            myNumbersStack.push(number);
+        }
+
+        Integer topPeekElement = (Integer) myNumbersStack.peek();
+
+        System.out.println("My Number Stack Top Peek Element : " + topPeekElement);
+
+        Integer myElementPositionInStack = (Integer) myNumbersStack.search(32);
+
+        System.out.println("My Element 32 Found at Position in Stack is : " + myElementPositionInStack);
+
+        // removing elements from the stack
+
+        System.out.println("Poping element from Stack is: " + myNumbersStack.pop());
+
+        System.out.println(myNumbersStack);
+
+    }
 
 
 }
