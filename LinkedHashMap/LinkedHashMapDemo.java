f94636079ccefb085596a5725a0ee033f2c0ee0b
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-30 18:30:47 -0600

    Linked HashMap Demo Example usage

---
+++ LinkedHashMap/LinkedHashMapDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Created by ganeshmadhu on 1/30/21.
+ */
+
+/**
+ * HashMap provided the advantage of quick insertion, search, and deletion
+ * but it never maintained the track and order of insertion which the LinkedHashMap
+ * provides where the elements can be accessed in their insertion order.
+ *
+ * A LinkedHashMap contains values based on the key.
+ * It contains only unique elements.
+ * It is non-synchronized.
+ * It  have one null key and multiple null values.
+ *
+ * It is the same as HashMap with an additional feature that it maintains insertion order.
+ */
+public class LinkedHashMapDemo {
+
+    public void linkedHashMapDemoExample() {
+
+
+
+    }
+
+}
