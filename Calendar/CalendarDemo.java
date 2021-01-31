1c55b69e5af0f77847aa4f33ee2e98df283dce16
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-31 09:37:03 -0600

    Calendar Demo Usage

---
+++ Calendar/CalendarDemo.java
@@ -0,0 +1,31 @@
+package com.jwetherell.algorithms.MyGit.Date;
+
+/**
+ * Created by ganeshmadhu on 1/30/21.
+ */
+
+/**
+ * set of calendar fields such as MONTH, YEAR, HOUR, etc.
+ *
+ * Calendar class in Java is an abstract class
+ *
+ * Calendar.getInstance()
+ *
+ * Calendar.getInstance(TimeZone zone)
+ *
+ * Calendar.getInstance(Locale aLocale)
+ *
+ * Calendar.getInstance(TimeZone zone, Locale aLocale)
+ *
+ * int get(int field)	It is used to return the value of the given calendar field.
+ *
+ * abstract int getMaximum(int field)	It is used to return the maximum value for the given calendar field of this Calendar instance.
+ *
+ * Date getTime()	It is used to return a Date object representing this Calendar’s time value.
+ *
+ * abstract int getMinimum(int field)	It is used to return the minimum value for the given calendar field of this Calendar instance.
+ */
+public class CalendarDemo {
+
+
+}
