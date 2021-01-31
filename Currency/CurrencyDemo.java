b471de85455cb6f103a83cb7ff224d75b6420972
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-31 09:55:59 -0600

    Currency Demo Usage

---
+++ Currency/CurrencyDemo.java
@@ -0,0 +1,20 @@
+/**
+ * Created by ganeshmadhu on 1/30/21.
+ */
+/**
+ * getCurrency() ==> method returns ISO 4217 currency code of the passed currency argument.
+ *
+ * getInstance() : java.util.Currency.getInstance() method creates currency instance for Currency code.
+ *
+ * getDefaultFractionDigits() : java.util.Currency.getDefaultFractionDigits() method returns default number of argumented currency fraction digits.
+ *
+ * getDisplayName() : java.util.Currency.getDisplayName() method generates the currency name of the argumented currency code.
+ *
+ * getSymbol() : java.util.Currency.getSymbol() method returns Currency symbol for the argumented currency code.
+ *
+ * */
+public class CurrencyDemo {
+
+    
+
+}
