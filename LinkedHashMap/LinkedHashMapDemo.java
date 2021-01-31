b0eacd22cb128bd7b8bb88bb5b089cdfeb64389d
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2021-01-30 18:53:18 -0600

    adding the linked hash map usage example

--- LinkedHashMap/LinkedHashMapDemo.java
+++ LinkedHashMap/LinkedHashMapDemo.java
@@ -2,6 +2,8 @@
  * Created by ganeshmadhu on 1/30/21.
  */
 
+import java.util.LinkedHashMap;
+
 /**
  * HashMap provided the advantage of quick insertion, search, and deletion
  * but it never maintained the track and order of insertion which the LinkedHashMap
@@ -18,7 +20,34 @@ public class LinkedHashMapDemo {
 
     public void linkedHashMapDemoExample() {
 
+        LinkedHashMap<String, String> linkedHashMap
+                = new LinkedHashMap<String, String>();
 
+        // Add mappings using put method
+        linkedHashMap.put("student1", "Patrick");
+        linkedHashMap.put("student2", "Jim");
+        linkedHashMap.put("student3", "John");
+
+        // It prints the elements in same order as they were inserted
+        System.out.println(linkedHashMap);
+
+        System.out.println("name of student1 : " + linkedHashMap.get("student1"));
+
+        System.out.println("name of student2 : " + linkedHashMap.get("student2"));
+
+        System.out.println("name of student3 : " + linkedHashMap.get("student3"));
+
+        System.out.println("size of map : " + linkedHashMap.size());
+
+        System.out.println("Is map empty? " + linkedHashMap.isEmpty());
+
+        System.out.println("is Map contains student4? " + linkedHashMap.containsKey("student4"));
+
+
+        System.out.println("delete element 'student1': " + linkedHashMap.remove("student1"));
+
+        // print mappings to the console
+        System.out.println("students  of LinkedHashMap : " + linkedHashMap);
 
     }
 
